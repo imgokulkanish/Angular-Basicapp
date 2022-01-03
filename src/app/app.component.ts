5f86d1fb0d6223b3d8f15f75387e93912afd916d
Gokul Kanish <66904387+imgokulkanish@users.noreply.github.com>
2022-01-03 22:00:37 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Cricbuzz';
+  imgurl = "assets/vk.jpg"
+  display = "Access Denied"
+  Name = ""
+  username1:string = "Enter Your Name";
+  // show : boolean=true;//ngIf
+  // allow = true;
+  onAllow(){
+    return this.display="Access Allowed";
+  }
+
+// constructor() {
+//   setTimeout(()=>{
+//     this.allow = false;
+//   },8000);
+// }
+OnUpdateName(event:Event){
+   return this.Name = (<HTMLInputElement>event.target).value;
+}
+ngOnInit(){
+}
+}
+
+  //ngStyle
+//   number:number;
+//   constructor () {
+//     this.number = Math.random() > 0.5 ? 0 : 1;
+//   }
+
+//   getnumber() {
+//     return this.number;
+//   }
+
+//   getColor(){
+//     return this.number === 0 ? 'green' : 'red';
+//   }
+// //structural directive-ngFor
+//   employees:Employee[]=[
+//     {name:"Gokul", dept:"MECH"},
+//     {name:"Swathy", dept:"ECE"},
+//     {name:"Balaji", dept:"CSE"},
+//     {name:"Vijay", dept:"MECH"}
+//   ]
+// }
+// class Employee{
+//   name!:string;
+//   dept!:string;
+// }
